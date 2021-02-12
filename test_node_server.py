df0865b42bdde7da18d6577e0b917638e693fe29
El-Adawy <51130800+el-adawy@users.noreply.github.com>
2021-02-12 14:00:58 +0100

    Update test_node_server.py

--- test_node_server.py
+++ test_node_server.py
@@ -1,9 +1,12 @@
 from node_server import blockchain
+from flask import json
 import requests
 import pytest
 
 
 def test_vote():
+	cpt = 0
+	cpt2= 0
 	url = 'http://localhost:5000/submit'
 	myobj = {'author': 'Paul', 'content': 'Joe Biden'}
 	response = requests.post(url, data = myobj)
@@ -13,15 +16,34 @@ def test_vote():
 	response = requests.get(url)
 	assert response.status_code == 200
 
+        #vérification
+	url = 'http://localhost:8000/chain'
+	response = requests.get(url)
+	assert response.status_code == 200
+
+
+	response_dict = json.loads(response.text)
+	for elt in response_dict['chain']:
+		print(elt['transactions'])
+		if (elt != []):
+			print(elt['transactions'])
+			for elt2 in elt['transactions']:
+				if( elt2['author'] == 'Paul' ):
+					cpt += 1
+				if ( elt2['content'] == 'Joe Biden'):
+					cpt2=1
+	assert cpt == 1
+	assert cpt2 == 1
+	
 def test_1():
 	cpt = 0
 	url = 'http://localhost:5000/submit'
-	myobj = {'author': 'Paul', 'content': 'Joe Biden'}
+	myobj = {'author': 'Jack', 'content': 'Joe Biden'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
 	url = 'http://localhost:5000/submit'
-	myobj = {'author': 'Paul', 'content': 'Emmanuel Macron'}
+	myobj = {'author': 'Jack', 'content': 'Emmanuel Macron'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
@@ -30,48 +52,53 @@ def test_1():
 	assert response.status_code == 200
         
         #vérification
-	for elt in blockchain.chain:
-		for tx in elt.transactions:
-			print(tx)
-			if (tx['author'] == 'Paul'):
-				cpt+=1
+	url = 'http://localhost:8000/chain'
+	response = requests.get(url)
+	assert response.status_code == 200
 
+
+	response_dict = json.loads(response.text)
+	for elt in response_dict['chain']:
+    		print(elt['transactions'])
+    		if (elt != []):
+        		print(elt['transactions'])
+        		for elt2 in elt['transactions']:
+            			if( elt2['author'] == 'Jack' ):
+                                    cpt+=1
 	assert cpt == 1
-    
-'''
+  
 
 def test_2():
 	cpt = 0
-	headers = {
-    'Content-type': 'application/json',
-}
-	data = "{'author' : 'Paul', 'content' : 'Joe Biden'}"
-	response = requests.post(
-            'http://localhost:5000/submit',
-                headers = headers,
-            data = data
-        )
-	response = requests.get(
-        'http://localhost:8000/mine'
-        )
-	response = requests.post(
-            'http://localhost:5000/submit',
-                headers = headers,
-            data = data
-        )
-	response = requests.get(
-        'http://localhost:8000/mine'
-        )
-
-
-
+	url = 'http://localhost:5000/submit'
+	myobj = {'author': 'Olaf', 'content': 'Joe Biden'}
+	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
-	for elt in blockchain.chain:
-		for tx in elt.transactions:
-			if (tx['author'] == 'Paul'):
-				cpt+=1
+	url = 'http://localhost:8000/mine'
+	response = requests.get(url)
+	assert response.status_code == 200
 
+	url = 'http://localhost:5000/submit'
+	myobj = {'author': 'Olaf', 'content': 'Emmanuel Macron'}
+	response = requests.post(url, data = myobj)
+	assert response.status_code == 200
+
+	url = 'http://localhost:8000/mine'
+	response = requests.get(url)
+	assert response.status_code == 200
+        
+        #vérification
+	url = 'http://localhost:8000/chain'
+	response = requests.get(url)
+	assert response.status_code == 200
+
+	response_dict = json.loads(response.text)
+	for elt in response_dict['chain']:
+    		print(elt['transactions'])
+    		if (elt != []):
+        		print(elt['transactions'])
+        		for elt2 in elt['transactions']:
+            			if( elt2['author'] == 'Jack' ):
+                                    cpt+=1
 	assert cpt == 1
-    #tester si y a un seul vote de Paul en parcourant la blockchain
-'''
