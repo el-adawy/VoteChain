92a348fe4b57644e669cba387fe6ea0c8310e20a
El-Adawy <51130800+el-adawy@users.noreply.github.com>
2021-02-12 14:42:44 +0100

    Update test_node_server.py

--- test_node_server.py
+++ test_node_server.py
@@ -7,17 +7,17 @@ import pytest
 def test_vote():
 	cpt = 0
 	cpt2= 0
-	url = 'http://localhost:5000/submit'
+	url = 'http://109.221.162.148:5000/submit'
 	myobj = {'author': 'Paul', 'content': 'Joe Biden'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
-	url = 'http://localhost:8000/mine'
+	url = 'http://109.221.162.148:8000/mine'
 	response = requests.get(url)
 	assert response.status_code == 200
 
         #vérification
-	url = 'http://localhost:8000/chain'
+	url = 'http://109.221.162.148:8000/chain'
 	response = requests.get(url)
 	assert response.status_code == 200
 
@@ -37,22 +37,22 @@ def test_vote():
 	
 def test_1():
 	cpt = 0
-	url = 'http://localhost:5000/submit'
+	url = 'http://109.221.162.148:5000/submit'
 	myobj = {'author': 'Jack', 'content': 'Joe Biden'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
-	url = 'http://localhost:5000/submit'
+	url = 'http://109.221.162.148:5000/submit'
 	myobj = {'author': 'Jack', 'content': 'Emmanuel Macron'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
-	url = 'http://localhost:8000/mine'
+	url = 'http://109.221.162.148:8000/mine'
 	response = requests.get(url)
 	assert response.status_code == 200
         
         #vérification
-	url = 'http://localhost:8000/chain'
+	url = 'http://109.221.162.148:8000/chain'
 	response = requests.get(url)
 	assert response.status_code == 200
 
@@ -70,26 +70,26 @@ def test_1():
 
 def test_2():
 	cpt = 0
-	url = 'http://localhost:5000/submit'
+	url = 'http://109.221.162.148:5000/submit'
 	myobj = {'author': 'Olaf', 'content': 'Joe Biden'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
-	url = 'http://localhost:8000/mine'
+	url = 'http://109.221.162.148:8000/mine'
 	response = requests.get(url)
 	assert response.status_code == 200
 
-	url = 'http://localhost:5000/submit'
+	url = 'http://109.221.162.148:5000/submit'
 	myobj = {'author': 'Olaf', 'content': 'Emmanuel Macron'}
 	response = requests.post(url, data = myobj)
 	assert response.status_code == 200
 
-	url = 'http://localhost:8000/mine'
+	url = 'http://109.221.162.148:8000/mine'
 	response = requests.get(url)
 	assert response.status_code == 200
         
         #vérification
-	url = 'http://localhost:8000/chain'
+	url = 'http://109.221.162.148:8000/chain'
 	response = requests.get(url)
 	assert response.status_code == 200
 
